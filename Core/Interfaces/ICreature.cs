a36ce57109007c3ec949ffd969d0299422f179c6
Denis Vashkevich <Dzianis_Vashkevich@epam.com>
2016-04-06 11:17:54 +0300

    Project added
    ICreature interface added

---
+++ Core/Interfaces/ICreature.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+	interface IInterface
+	{
+		int Id { get; set; }
+		string Name { get; set; }
+		int HitPointsTotal { get; set; }
+		int HitPoints { get; set; }
+		int Damage { get; set; }
+		double MovesTotal { get; set; }
+		double Moves { get; set; }
+	}
+}
